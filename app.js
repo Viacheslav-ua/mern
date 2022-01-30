05584b9675101a4f4205023fa5395cc6ce78615d
Viacheslav-ua <atph@ukr.net>
2022-01-30 08:25:54 +0200

    finish

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -7,6 +8,14 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./route/auth.routes'))
 app.use('/api/link', require('./route/link.routes'))
 app.use('/t', require('./route/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join('__dirname', 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 3000
 
